7cca4e8c3b166259176f0b8d9f681739ce5edca6
lvcky-gg <matadorcrab@gmail.com>
2024-04-24 19:21:53 -0400

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,26 @@
 </script>
 
 <template>
-  <div>
+  <div class="test" >
+    <p v-for="i in 100">Hello</p>
+
    
   </div>
 
 </template>
+<style scoped>
+  .test {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  z-index: 1;
+  position: absolute;
+  background-color: rgba(71, 71, 71, 0.377);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow-y: scroll;
+  }
+</style>
